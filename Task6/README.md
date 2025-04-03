2ad44810f90a34815edf8afb9c73082dd77e84e1
Nithishvikky <nithishudhayakumar007@gmail.com>
2025-04-03 12:44:48 +0530

    README file added

---
+++ Task6/README.md
@@ -0,0 +1,14 @@
+# Drag and Drop List Reordering
+
+## Overview
+ Implemented a list that allows users to reorder items using drag and drop functionality.
+
+## New Things Learned
+- Learned about **Drag and Drop API** and Used methods such as `dragenter`,`dragstart`,`dragover` and `dragend`.
+- Used methods such as `clientY`,`offsetTop` and `offsetHeight` which will give the element's details.
+- To update the DOM dynamically I used `insertBefore` which will help to arrange list items.
+
+## Outputs
+
+![List](Outputs/list.png)
+![Reordering](Outputs/ordering.png)
--- Task6/script.js
+++ Task6/script.js
@@ -20,14 +20,16 @@ list.addEventListener("dragover",(e)=>{
     let reorderingItem = otherItems.find(hoveringItem =>{
         console.log(hoveringItem);
 
-        //  It is the Y axis position of item's middle relative to viewport (hoveringItem.offsetTop + hoveringItem.offsetHeight / 2)
+        //  It is the Y axis position of item's middle relative to nearest positioned ancestor (hoveringItem.offsetTop + hoveringItem.offsetHeight / 2)
+        // Here these items don't have nearest ancestor so it will take body as ancestor and work relative to it.
         // It is the Y axis position of mouse realtive to viewport (e.clientY)
         // That means Comparing two Y axis position if mouse's is greater than items's it will return that item.
 
         return e.clientY <= hoveringItem.offsetTop + hoveringItem.offsetHeight / 2;
     });
     console.log(reorderingItem);
-
+    
+    // It will update the dom to reflect new order
     list.insertBefore(draggingItem,reorderingItem);
 })
 
